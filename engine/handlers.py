a0e6971d1072c42c410efa6843c287b633769c77
Igor Nekludov <igor.nekludovv@gmail.com>
2024-07-05 16:16:35 +0300

    исправил ошибку с коллбэками у кнопок

--- engine/handlers.py
+++ engine/handlers.py
@@ -25,9 +25,27 @@ class MyHandler(CallbackQueryHandler):
         user_choice = self.data['event_update'].callback_query.data
         if user_choice == 'home': # inline на главную
             await self.message.edit_text(data["greeting_message2"], reply_markup=await kb.menu())
-        for i in data['menu']: # inline категория
-            if user_choice == str(i):
-                await self.message.edit_text(str(i).upper(), reply_markup=await kb.keyboard(i))
-            for j in data['menu'][i]: # inline тема в категории
-                if user_choice == str(j):
-                    await self.message.edit_text(str(data['menu'][i][j]), reply_markup=kb.other_kb.as_markup())
+
+        if user_choice == 'other':
+            await self.message.edit_text(data['other_message'], reply_markup=kb.back_kb.as_markup())
+
+        if user_choice.count("_") == 0:
+            for i, button in enumerate(data['menu']): #inline category
+                if user_choice == str(i):
+                    await self.message.edit_text(str(button).upper(), reply_markup=await kb.keyboard(i, button))
+        else:
+            for i, button in enumerate(data['menu']):
+                for j, buttn in enumerate(data['menu'][button]):
+                    if user_choice == str(i) + "_" + str(j):
+                        await self.message.edit_text(str(data['menu'][button][buttn]), reply_markup=kb.other_kb.as_markup())
+       # else:
+        #    for i, button in enumerate(data['menu']):
+
+
+
+        #for i in data['menu']: # inline категория
+        #    if user_choice == str(i):
+        #        await self.message.edit_text(str(i).upper(), reply_markup=await kb.keyboard(i))
+        #    for j in data['menu'][i]: # inline тема в категории
+        #        if user_choice == str(j):
+        #            await self.message.edit_text(str(data['menu'][i][j]), reply_markup=kb.other_kb.as_markup())
--- engine/keyboards.py
+++ engine/keyboards.py
@@ -10,18 +10,22 @@ other_kb = InlineKeyboardBuilder()
 other_kb.add(InlineKeyboardButton(text='Другое', callback_data='other'))
 other_kb.add(InlineKeyboardButton(text='На главную', callback_data='home'))
 
+back_kb = InlineKeyboardBuilder()
+back_kb.add(InlineKeyboardButton(text='Назад', callback_data='home'))
 
 async def menu():
     keyboard = InlineKeyboardBuilder()
-    for i in data['menu']:
-        keyboard.add(InlineKeyboardButton(text=str(i), callback_data=str(i)))
+    for i, button in enumerate(data['menu']):
+        keyboard.add(InlineKeyboardButton(text=str(button), callback_data=str(i)))
+    keyboard.add(InlineKeyboardButton(text='Другое', callback_data='other'))
     return keyboard.adjust(1).as_markup()
 
 
-async def keyboard(i):
+async def keyboard(enum, buttn):
     keyboard = InlineKeyboardBuilder()
-    for i in data['menu'][i]:
-        keyboard.add(InlineKeyboardButton(text=str(i), callback_data=str(i)))
+    for i, button in enumerate(data['menu'][buttn]):
+        keyboard.add(InlineKeyboardButton(text=str(button), callback_data=str(enum) + "_"+str(i)))
+    keyboard.add(InlineKeyboardButton(text='Другое', callback_data='other'))
     keyboard.add(InlineKeyboardButton(text='На главную', callback_data='home'))
     return keyboard.adjust(1).as_markup()
 
