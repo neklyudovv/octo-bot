9d4f4b2beb3d46a2b8178860a89df1c032093d70
Dmitry Sidorov <61616100+JustyDev@users.noreply.github.com>
2024-11-18 00:56:09 +0300

    up

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,9 @@
 python3 -m venv .venv
 source .venv/bin/activate
 
+pip install python-dotenv
+pip install aiogram
+pip install google-api-python-client
+
 # Запускаем основной скрипт
 python3 main.py
